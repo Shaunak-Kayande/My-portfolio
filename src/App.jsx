92d8ecdb4a30af693e742080be9bb719c0775c1d
Shaunak-Kayande <shaunakk.2801@gmail.com>
2023-09-13 16:01:26 +0530

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useRef, useState } from "react";
-import bgtemp from "./assets/img/bg-temp.png";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
